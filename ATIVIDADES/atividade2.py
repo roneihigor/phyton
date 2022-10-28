de497ff341b0197c55128dd0675f2df9c97029f5
Ronei Higor Ferreira Matias <roneihigor0701@gmail.com>
2022-10-27 21:45:37 -0300

    atividade2

---
+++ ATIVIDADES/atividade2.py
@@ -0,0 +1,4 @@
+#  Crie um programa para realizar as operações de uma tabuada de multiplicação
+#  onde será solicitado ao usuário digitar qual numero será a tabuada e qual intervalo do inicio e fim da tabuada
+#  e exibir na tela o resultado conforme intervalo.
+
---
+++ ATIVIDADES/atividade3.py
@@ -0,0 +1,3 @@
+# • Crie um programa de calculadora que realiza as operações de soma, multiplicação, divisão e subtração
+# será perguntado ao usuário se deseja continuar com o uso da calculadora enquanto ele digitar (“S” – Sim) o programa irá reiniciar a calculadora.
+
